5e31cf2ac27a47af2fd934deb37d4bbcde46657a
Arya Aditya <aryaeggyaditya@gmail.com>
2025-04-24 20:33:15 +0700

    Added message

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // config.php
-define('GROQ_API_KEY', 'here'); // Ganti dengan API Key Anda
+define('GROQ_API_KEY', 'here_(old API revoked)'); // Ganti dengan API Key Anda
 define('GROQ_API_URL', 'https://api.groq.com/openai/v1/chat/completions'); // Endpoint URL dari dokumentasi
 ?>
